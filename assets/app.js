67386757661303158ac32be96c9595ae6b792840
maszity <aszitymilan@gmail.com>
2026-03-09 17:42:09 +0100

    CSV adat konverter segéd metódus

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,35 @@
+function CSVKonverter (csv) {
+
+    const sorok     = csv.trim().split('\n');
+    const fejlec    = sorok[0].split('\t');
+    const eredmeny  = [];
+
+    for (let i = 1; i < sorok.length; i++) {
+
+        const obj           = {};
+        const jelenlegiSor  = sorok[i].split('\t');
+
+        for (let j = 0; j < fejlec.length; j++) {
+
+            obj[fejlec[j].trim()] = jelenlegiSor[j].trim();
+
+        }
+
+        eredmeny.push(obj);
+    }
+
+    return eredmeny;
+
+}
+
 async function tablaBeolvasasa (tablaUtvonal) {
 
-    const olvaso = await fetch(tablaUtvonal);
-    
-    olvaso
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
+    const olvaso = fetch(tablaUtvonal)
+    .then((response) => response.text())
+    .then((adat) => {
+        
+        console.log(CSVKonverter(adat))
+
     });
 
 }
