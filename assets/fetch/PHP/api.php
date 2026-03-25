3370a1794f9a198680d961188e7f31c816d86241
maszity <aszitymilan@gmail.com>
2026-03-25 19:00:06 +0100

    Süti mentése funkció

--- assets/fetch/PHP/api.php
+++ assets/fetch/PHP/api.php
@@ -8,13 +8,40 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
 
     case 'GET':  
+
         try {
+
             $stmt = $pdo->query("SELECT * FROM sutik");
             $readData=$stmt->fetchAll();
             echo json_encode(['success' => true, "data" => $readData]);
+
         }
         catch(PDOException $e) {
+
           echo json_encode(['success' => false, 'error' => $e->getMessage()]);
+
+        }
+
+    break;
+
+    case 'POST':
+
+        $data = json_decode(file_get_contents("php://input"), true) ?? $_POST;
+
+        try {
+
+            $stmt = $pdo->prepare("INSERT INTO sutik (nev, tipus, dijazott) VALUES (?, ?, ?)");
+            $stmt->execute([
+                $data['nev'], $data['tipus'], !$data['dijazott'] ?? true
+            ]);
+
+            echo json_encode(['success' => true, 'message' => "Süti sikeresen mentve!", 'data' => $data]);
+
+        }
+        catch (PDOException $e) {
+
+            echo json_encode(['success' => false, 'error' => $e->getMessage()]);
+
         }
 
     break;
--- assets/fetch/app.js
+++ assets/fetch/app.js
@@ -1,12 +1,39 @@
-const API  = "/assets/fetch/PHP/api.php";
-const FORM = document.getElementById("suti-letrehozas-kontener"); 
+const API       = "/assets/fetch/PHP/api.php";
+const FORM      = document.getElementById("suti-letrehozas-kontener"); 
+const MESSAGE   = document.getElementById("message");
+
+window.onload = function() {
+    fetchSuti();
+};
+
+document.getElementById("suti-felvetele").addEventListener("click", sutiMentes);
 
 function sutiMentes () {
 
-    const sutiAdat = new FormData(FORM);
+    const nev       = document.getElementById("suti-neve").value;
+    const tipus     = document.getElementById("suti-tipusa").value;
+    const dijazott  = document.getElementById("suti-dijazott").checked;
+
+
+    if (nev.length < 1 || tipus.length < 1) {
+
+        MESSAGE.innerText = "Minden mező kitöltése kötelező!";
+        MESSAGE.style.color = "red";
+
+        return;
+
+    }
+
+    const sutiAdat = {
+
+        nev      : nev,
+        tipus    : tipus,
+        dijazott : dijazott
+
+    };
 
     fetch(API, {
-        method  : POST,
+        method  : "POST",
         headers : {"Content-Type": "application/json"},
         body    : JSON.stringify(sutiAdat)
     })
@@ -14,10 +41,29 @@ function sutiMentes () {
     .then(data => {
 
         FORM.reset();
-        document.getElementById("message").innerText = data.status;
 
-        console.log(data);
-        // fetchSuti();
+        if (data.success == true) {
+
+            MESSAGE.innerText = data.message;
+            MESSAGE.style.color = "green";
+
+        }
+        else {
+
+            MESSAGE.innerText = "Hiba történt a folyamat közben: " + data.error;
+            MESSAGE.style.color = "red";
+
+        }
+
+        fetchSuti();
+
+    })
+    .catch(error => {
+
+        MESSAGE.innerText = "Hiba történt a folyamat közben!";
+        MESSAGE.style.color = "red";
+
+        console.log(error)
 
     });
 
@@ -46,7 +92,16 @@ function fetchSuti () {
                     <td>${suti.nev}</td>
                     <td>${suti.tipus}</td>
                     <td>${suti.dijazott === 0 ? 'Nem' : 'Igen'}</td>
-                    <td></td>
+                    <td>
+                        <div class="flex row align-center justify-center gap-1">
+                            <div class="card-badge pointer" data-id="${suti.id}" suti-akcio="suti-modositas" title="Sütemény szerkesztése">
+                                <svg width="1rem" height="1rem" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path stroke="#0042aa" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.8 12.963L2 18l4.8-.63L18.11 6.58a2.612 2.612 0 00-3.601-3.785L3.8 12.963z"></path> </g></svg>
+                            </div>
+                            <div class="card-badge pointer" data-id="${suti.id}" suti-akcio="suti-torles" title="Sütemény törlése">
+                                <svg width="1rem" height="1rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M14 10V17M10 10V17" stroke="#ff2600" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                            </div>
+                        </div>
+                    </td>
                 </tr>
             `;
 
@@ -56,5 +111,3 @@ function fetchSuti () {
     });
 
 }
-
-fetchSuti();
